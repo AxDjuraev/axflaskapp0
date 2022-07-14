6993e5a0113fdf546597f4fe6957c5ed6a7c360b
root <axdjuraev@gmail.com>
2022-07-14 16:01:26 +0000

    modify app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 
 from flask import Flask
 
-app = Flask(__main__)
+app = Flask(__name__)
 @app.route('/')
 def home():
-  return 'hello, again'
+  content = f'hello, again'
+  return content
 
 if __name__ == '__main__':
   app.run()
