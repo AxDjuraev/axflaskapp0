e825cdebe1baad2c9991c433d5ceb88a2357852e
root <axdjuraev@gmail.com>
2022-07-15 18:31:22 +0000

    adding render_template to app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask, render_template
 application = Flask(__name__, template_folder='templates')
 @application.route("/")
 def home():
-  content = f'hello, again'
-  return content
+  content = render_template('index.html')
+  return content 
 
 if __name__ == '__main__':
   application.run(debug=True)
