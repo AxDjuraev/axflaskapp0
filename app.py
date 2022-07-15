1319cf1ddcf7a562b5b639b3856f8270907e0aab
root <axdjuraev@gmail.com>
2022-07-15 17:34:48 +0000

    modify app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 
 from flask import Flask
 
-app = Flask(__name__, template_folder='templates'
-@app.route('/')
+application = Flask(__name__, template_folder='templates')
+@application.route("/")
 def home():
   content = f'hello, again'
   return content
 
 if __name__ == '__main__':
-  app.run()
+  application.run()
