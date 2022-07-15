98dcdab7598785c0c55b1c193bb272fde8127533
root <axdjuraev@gmail.com>
2022-07-15 19:18:38 +0000

    adding home route

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template, url_for
 
 application = Flask(__name__, template_folder='templates')
 @application.route("/")
-def home():
+def main():
   content = render_template('index.html')
+  return content 
+
+@application.route("/home")
+def home():
+  content = render_template('home.html')
   return content 
 
 if __name__ == '__main__':
